cc5d622dc5bf5a6b406693cff1dfb032087dfb16
Akash Tripathi <51534948+akashtrip15@users.noreply.github.com>
2022-10-17 15:08:37 +0800

    Time: 1 ms (85.62%), Space: 51.7 MB (17.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int sum =0;
-        Arrays.sort(nums);
         for(int i =0; i<nums.length; i++){
-            if(i!=nums[i]){
-                return i;
-            }
+            sum+=nums[i];
         }
         
         
-        return nums.length;
+        int maxSum = (nums.length*(nums.length+1))/2;
+        return maxSum-sum;
     }
 }
